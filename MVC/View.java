8292c034bcd379baf1179724b71b5d0b99eafcb9
Tochilin-Andrey <51118194+Axizerq@users.noreply.github.com>
2020-04-11 22:45:53 +0300

    Update View.java

--- MVC/View.java
+++ MVC/View.java
@@ -2,25 +2,21 @@
 
 public class View {
 
-    // Constructor
     public View(Model model) {
         printText();
     }
 
-    // Method printText
     public void printText() {
-        System.out.print("P.S. TYPE \"exit\" TO EXIT THIS PROGRAM!!! \nPlease enter your " +
-                "favorite number or type \"exit\": ");
+        System.out.print("\"exit\" to exit this program! \nPlease enter your " +
+                "number: ");
     }
 
-    // Method printText2
     public void printText2() {
-        System.out.print("P.S. TYPE \"exit\" TO EXIT THIS PROGRAM!!! \nIf you want you can " +
-                "enter another favorite number or type \"exit\": ");
+        System.out.print("\"exit\" to exit this program! \nIf you want you can " +
+                "enter another number: ");
     }
 
-    // Method printNum
     public void printNum(int num) {
-        System.out.println("\nYour favorite number is: " + num + "\n");
+        System.out.println("\nYour number is: " + num + "\n");
     }
 }
