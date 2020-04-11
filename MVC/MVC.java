e082a8f58956e34ebb4fba51362c652b306739c5
Tochilin-Andrey <51118194+Axizerq@users.noreply.github.com>
2020-04-11 22:41:17 +0300

    Update MVC.java

--- MVC/MVC.java
+++ MVC/MVC.java
@@ -4,9 +4,9 @@ public class MVC {
 
     public static void main(String[] args) throws IOException {
         // model
-        ModelEx model = new Model();
+        Model model = new Model();
         // view
-        ViewEx view = new View(model);
+        View view = new View(model);
         // Pass to Controller
         new Controller(model, view).run();  
     }
