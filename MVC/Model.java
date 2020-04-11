a0f81224156a355d28f4e1b360c28285e047b5aa
Tochilin-Andrey <51118194+Axizerq@users.noreply.github.com>
2020-04-11 22:46:52 +0300

    Update Model.java

--- MVC/Model.java
+++ MVC/Model.java
@@ -1,19 +1,15 @@
 public class Model {
 
-    // Attributes
     private int num;
 
-    // Constructor
     public Model() {
         this.yourNum();
     }
 
-    // Getters
     public int getNum() {
         return this.num;
     }
 
-    // Method
     public int yourNum() {
         return this.num;
     }
