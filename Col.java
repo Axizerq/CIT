6999fafce9c798b248f30169e984194a4ce40ab8
Tochilin-Andrey <51118194+Axizerq@users.noreply.github.com>
2021-05-01 04:03:58 +0300

    Update Col.java

--- Col.java
+++ Col.java
@@ -1,37 +1,30 @@
+import java.util.*;
+
 public class Col {
- 
+
 	public static void main(String[] args) {
-		
-		Map();
-		Tree();
-		List();
-		Set();
-	}
 
-	
-	public static void Map() {
-		
-		Map iMap = new HashMap();
-		iMap.put("Russia", "1");
-		iMap.put("Ukraine", "2");
-		iMap.put("France", "3");
-		iMap.put("Spain", "4");
-		iMap.put("Swiden", "5");
-		iMap.put("Norway", "6");
-		iMap.put("Spain", "4");
-		System.out.println("The largest countries in Europe (HashMap)");
-		System.out.println("\t" + iMap);
-	
-		iMap.remove("Norway");
-		System.out.println("\t" + iMap);
-		
-		iMap.put("Germany", "7");
-		System.out.println("\t" + iMap);
-	}
 
-	
-	public static void Tree() {
-		TreeSet iTree = new TreeSet();
+		Map<Integer, String> iMap = new HashMap<Integer, String>();
+		iMap.put(1, "Russia");
+		iMap.put(2, "Ukraine");
+		iMap.put(3, "France");
+		iMap.put(4, "Spain");
+		iMap.put(5, "Swiden");
+		iMap.put(6, "Norway");
+		System.out.println("The largest countries in Europe");
+		System.out.println("\t" + iMap);
+
+		iMap.remove(6, "Norway");
+		System.out.println("\t" + iMap);
+
+		iMap.put(6, "Germany");
+		System.out.println("\t" + iMap);
+
+		Collection<String> values = iMap.values();
+
+
+		TreeSet<String> iTree = new TreeSet<String>();
 		iTree.add("Black Ash");
 		iTree.add("Basswood");
 		iTree.add("Paper Birch");
@@ -39,11 +32,12 @@ public class Col {
 		System.out.println();
 		System.out.println("Few types of trees");
 		System.out.println("\t" + iTree);
-	}
-	
 
-	public static void List() {
-		List iList = new ArrayList<>();
+		System.out.printf("TreeSet contains %d elements \n", iTree.size());
+
+
+
+		List iList = new ArrayList();
 		iList.add("Andromeda");
 		iList.add("Black Eye Galaxy");
 		iList.add("Bode's Galaxy");
@@ -53,26 +47,24 @@ public class Col {
 		System.out.println();
 		System.out.println("There is a few galaxies names:");
 		System.out.println("\t" + iList);
-		
+
 		iList.remove("Solar System");
 		System.out.println("Remove the Solar System - it's not a galaxy!");
 		System.out.println("\t" + iList);
-		
+
 		iList.add("Milky Way");
 		System.out.println("Added the Milky Way - that's correct.");
 		System.out.println("\t" + iList);
-		
+
 		Iterator iterator = iList.iterator();
 		while(iterator.hasNext()) {
 			String iString = (String) iterator.next();
 			System.out.println(iString);
 		}
-	}
-	
 
-	public static void Set() {
-		Set iSet = new LinkedHashSet();
-		iSet.add("Denver Broncose");
+
+		HashSet<String> iSet = new HashSet<>();
+		iSet.add("Denver Broncos");
 		iSet.add("Baltimore Ravens");
 		iSet.add("New England Patriots");
 		iSet.add("Philadelphia Eagles");
@@ -80,42 +72,45 @@ public class Col {
 		System.out.println();
 		System.out.println("NFL teams");
 		System.out.println("\t" + iSet);
-		
+
 		System.out.println("Removed San Diego Chargers");
-		iSet.remove("Goblet of Fire");
+		iSet.remove("San Diego Chargers");
 		System.out.println("\t" + iSet);
-		
+
 		System.out.println("Added Los Angeles Chargers");
 		iSet.add("Los Angeles Chargers");
 		System.out.println("\t" + iSet);
-		
+
 		// Sets do not accept duplicate items:
-                iSet.add("Baltimore Ravens");
-                iSet.add("Philadelphia Eagles");
-                System.out.println("You can't add the same value to the list" + iSet);
-		
-		Iterator iterator = iSet.iterator();
-		while(iterator.hasNext()) {
-			String iString = (String) iterator.next();
-			System.out.println(iString);
+		iSet.add("Baltimore Ravens");
+		iSet.add("Philadelphia Eagles");
+		System.out.println("You can't add the same value to the list " + iSet);
+
+
+
+		PriorityQueue<Integer> iQueue = new PriorityQueue<Integer>();
+
+		iQueue.add(1);
+		iQueue.add(2);
+		iQueue.add(3);
+
+		for(int i: iQueue) {
+			System.out.println(i);
 		}
-	
-	public static void Queue() {
-		PriorityQueue iQueue = new PriorityQueue();
-		 
-                iQueue.add(1);
-                iQueue.add(2);
-                iQueue.add(3);
- 
-                for(int i: iQueue)
-                   System.out.println(i);
-                iQueue.remove(2);
-                System.out.println("After removing:");
-                for(int i: iQueue)
-                   System.out.println(i);
-        
-                iQueue.offer(4);
-                   for(int i: iQueue)
-            System.out.println(i);
+		System.out.println();
+
+		iQueue.remove(2);
+		System.out.println("After removing:");
+		for(int i: iQueue) {
+			System.out.println(i);
+		}
+		System.out.println();
+
+		System.out.println("After being added to the queue:");
+		iQueue.offer(4);
+		for(int i: iQueue) {
+			System.out.println(i);
+		}
 	}
+
 }
